cd01abe16a2d2c873faab24bab6ce7667d72fa37
Anton Kargapolov <igloooooook@gmail.com>
2023-06-29 16:23:28 +0500

    function for do second array

--- final_task/Program.cs
+++ final_task/Program.cs
@@ -13,3 +13,16 @@ for (int i = 0; i < size; i++)
 {
     array1[i] = Console.ReadLine();
 }
+
+void SecondArray(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+        if (array1[i].Length <= 3)
+        {
+            array2[count] = array1[i];
+            count++;
+        }
+    }
+}
